667ddc6207696dd72cdc6b8ad07d5bda8447d8ce
Beyza <141168063+Beyzajjk@users.noreply.github.com>
2024-04-29 16:19:32 +0300

    This is a code about user inputs
    This is basically about entering your Name and Surname and your interests, and with ADD button you are saving your name and surname to our class. Then with our listview with SHOW button, that saved name and surname will be showing.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace Application1;
+
+public partial class MainPage : ContentPage
+{
+	public List<NameSurname> NameSurnameList = new List<NameSurname>();
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+
+    private void Add_Button_Clicked(object sender, EventArgs e)
+    {
+       
+        NameSurnameList.Add(new NameSurname(StuName.Text, StuSurname.Text, PhoneNum.Text));
+    }
+
+    private void Show_Button_Clicked(object sender, EventArgs e)
+    {
+        Name_Surname_View.ItemsSource = null;
+
+        Name_Surname_View.ItemsSource = NameSurnameList;
+    }
+}
+
---
+++ NameSurname.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application1
+{
+    public class NameSurname
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; } 
+        
+        public string PhoneNumber { get; set; }
+
+        public string Gender { get; set; }
+
+        public string CheckBox { get; set; }
+
+        public NameSurname(string name, string surname, string phoneNumber)
+        {
+            Name = name;
+            Surname = surname;
+            PhoneNumber = phoneNumber;
+        }
+
+
+
+    }
+}
